0b139ba874445dd020ffeffb164e32c4b2b5912f
zsxwz <34711528+zsxwz@users.noreply.github.com>
2020-10-12 19:23:11 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# Download and install V2Ray
+mkdir /tmp/v2ray
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+
+# Remove temporary directory
+rm -rf /tmp/v2ray
+
+# V2Ray new configuration
+install -d /usr/local/etc/v2ray
+cat << EOF > /usr/local/etc/v2ray/config.json
+{
+  "log": {
+    "access": "/var/log/v2ray/access.log",
+    "error": "/var/log/v2ray/error.log",
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "port": $PORT,
+      "tag": "VLESS-in",
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+          "id": "$UUID"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/$PATH"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ]
+}
+EOF
+
+# Run V2Ray
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
